37c5eb3e26f64e2c0ccbe2f3515d3b9c32a36095
Victor Moccand <victor.moccand@gmail.com>
2019-03-29 21:32:39 +0100

    add permission Location

--- app/src/main/java/com/atomikrobot/atomikweather/MainActivity.java
+++ app/src/main/java/com/atomikrobot/atomikweather/MainActivity.java
@@ -1,13 +1,122 @@
 package com.atomikrobot.atomikweather;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 0;
+    private Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activity = this;
+
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
+        Button cancelButton = (Button) findViewById(R.id.button_cancel);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckPermission();
+            }
+        });
+
+        CheckPermission();
+
     }
+
+
+
+
+
+    /**
+     * check if permission ok else make a demand
+     */
+    public void CheckPermission () {
+
+        //Check if permission location is ok in the manifest
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                builder.setTitle(this.getString(R.string.cancel_text));
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        ActivityCompat.requestPermissions( activity , new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  }, MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION );
+                    }
+                });
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        cancelLocation();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            } else {
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+            }
+        }else {
+            okLocation();
+        }
+
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    okLocation();
+                } else {
+                    cancelLocation();
+                }
+                break;
+            }
+        }
+    }
+
+
+    private void okLocation () {
+        progressBar.setVisibility(View.GONE);
+        TextView okText = (TextView) findViewById(R.id.okLocationText);
+        okText.setVisibility(View.VISIBLE);
+    }
+
+    private void cancelLocation () {
+        progressBar.setVisibility(View.GONE);
+        LinearLayout cancelLayout = (LinearLayout) findViewById(R.id.layout_cancel);
+        cancelLayout.setVisibility(View.VISIBLE);
+    }
+
+
 }
