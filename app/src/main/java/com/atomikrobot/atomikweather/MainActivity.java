2ec59cf4214ddeccc499a92d760e1c3ac161eb66
AtomikRobot <victor.moccand@gmail.com>
2019-03-30 10:02:47 +0100

    add location of the phone

--- app/src/main/java/com/atomikrobot/atomikweather/MainActivity.java
+++ app/src/main/java/com/atomikrobot/atomikweather/MainActivity.java
@@ -1,6 +1,7 @@
 package com.atomikrobot.atomikweather;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -8,6 +9,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.audiofx.Equalizer;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -22,10 +28,20 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class MainActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 0;
     private Activity activity;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +61,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                TextView okText = (TextView) findViewById(R.id.okLocationText);
+                okText.setText(location.getLatitude()+"  "+location.getLongitude());
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+
         CheckPermission();
+
+
 
     }
 
+
+    @SuppressLint("MissingPermission")
+    private void LocationInit () {
+        locationManager.requestLocationUpdates("gps", 5000, 50, locationListener);
+    }
 
 
 
@@ -69,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
                 builder.setTitle(this.getString(R.string.cancel_text));
                 builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        ActivityCompat.requestPermissions( activity , new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  }, MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION );
+                        ActivityCompat.requestPermissions( activity , new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION  }, MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION );
                     }
                 });
                 builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -81,9 +132,10 @@ public class MainActivity extends AppCompatActivity {
                 AlertDialog dialog = builder.create();
                 dialog.show();
             } else {
-                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
             }
         }else {
+            LocationInit();
             okLocation();
         }
 
@@ -96,6 +148,7 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    LocationInit();
                     okLocation();
                 } else {
                     cancelLocation();
